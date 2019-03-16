149636e269ecfb57b4d3eb600386c60a3ab6af3d
Shoaib <mshoaibmemon@outlook.com>
2019-03-16 14:35:08 -0500

    Configured Amplify

---
+++ src/components/MainForm.jsx
@@ -0,0 +1,15 @@
+// MainForm.jsx
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+
+class MainForm extends Component {
+
+    render() {
+        return (
+            <div> </div>
+        );
+      }
+}
+
+export default MainForm;
